77966781cf90ee180f9f5b80e4d753388dcf9273
muhsinking <muhsinking@gmail.com>
2017-07-24 10:46:27 -0700

    Add spriteclass and camera classes

---
+++ Camera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+
+namespace OneWeek2017
+{
+	public static class Camera
+	{
+		public static float X = 0;
+		public static float Y = 0;
+		public static float speed = 300f;
+		public static float scale = 2f;
+
+		// TODO make this non framerate dependent
+		public static void HandleInput(float elapsedTime)
+		{
+			KeyboardState state = Keyboard.GetState();
+			if (state.IsKeyDown(Keys.Up))
+			{
+				Y -= speed * elapsedTime;
+			}
+			if (state.IsKeyDown(Keys.Down))
+			{
+				Y += speed * elapsedTime;
+			}
+			if (state.IsKeyDown(Keys.Left))
+			{
+				X -= speed * elapsedTime;
+			}
+			if (state.IsKeyDown(Keys.Right))
+			{
+				X += speed * elapsedTime;
+			}
+		}
+	}
+}
---
+++ SpriteClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace OneWeek2017
+{
+	public class SpriteClass
+	{
+		public Texture2D texture { get; set; }
+		public float X { get; set; }
+		public float Y { get; set; }
+		public float dX { get; set; }
+		public float dY { get; set; }
+		public float dA { get; set; }
+		public float angle { get; set; }
+		public float scale { get; set; }
+		public float hitboxScale { get; set; }
+
+		public SpriteClass(ContentManager content, string textureName)
+		{
+			X = 0;
+			Y = 0;
+			dX = 0;
+			dY = 0;
+			angle = 0f;
+			scale = 1f;
+			hitboxScale = 1f;
+
+			texture = content.Load<Texture2D>(textureName);
+		}
+
+		public SpriteClass(ContentManager content, string textureName, float X, float Y, float scale)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.dX = 0;
+			this.dY = 0;
+			this.angle = 0;
+			this.scale = scale;
+			hitboxScale = 1f;
+
+			texture = content.Load<Texture2D>(textureName);
+		}
+
+		public SpriteClass(ContentManager content, string textureName, float scale)
+		{
+			this.X = 0;
+			this.Y = 0;
+			this.dX = 0;
+			this.dY = 0;
+			this.angle = 0;
+			this.scale = scale;
+			hitboxScale = 1f;
+			texture = content.Load<Texture2D>(textureName);
+		}
+
+		public SpriteClass(ContentManager content, string textureName, float X, float Y, float scale, float angle)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.dX = 0;
+			this.dY = 0;
+			this.angle = angle;
+			this.scale = scale;
+			hitboxScale = 1f;
+			texture = content.Load<Texture2D>(textureName);
+		}
+
+		public void Update(float elapsedTime)
+		{
+			X += dX * elapsedTime;
+			Y += dY * elapsedTime;
+			angle += dA * elapsedTime;
+			scale = Camera.scale;
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			Vector2 spritePosition = new Vector2(X + Camera.X, Y + Camera.Y);
+			//Vector2 spritePosition = new Vector2(X, Y);
+			spriteBatch.Draw(texture, spritePosition, null, Color.White, angle, new Vector2(texture.Width / 2, texture.Height / 2), new Vector2(scale, scale), SpriteEffects.None, 0f);
+		}
+
+		public Boolean RectangularCollision(SpriteClass otherSprite)
+		{
+			if (this.X + this.texture.Width * this.scale * this.hitboxScale / 2 <
+				otherSprite.X - otherSprite.texture.Width * otherSprite.scale * otherSprite.hitboxScale / 2) return false;
+			if (this.Y + this.texture.Height * this.scale * this.hitboxScale / 2 <
+				otherSprite.Y - otherSprite.texture.Height * otherSprite.scale * otherSprite.hitboxScale / 2) return false;
+			if (this.X - this.texture.Width * this.scale * this.hitboxScale / 2 >
+				otherSprite.X + otherSprite.texture.Width * otherSprite.scale * otherSprite.hitboxScale / 2) return false;
+			if (this.Y - this.texture.Height * this.scale * this.hitboxScale / 2 >
+				otherSprite.Y + otherSprite.texture.Height * otherSprite.scale * otherSprite.hitboxScale / 2) return false;
+			return true;
+		}
+	}
+}
