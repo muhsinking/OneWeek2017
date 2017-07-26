95fdc99eac24623663df3c410c1016eadf57bd62
Danny Lewis <nitomatik@gmail.com>
2017-07-26 11:18:34 -0700

    LevelGenerator First pass

---
+++ Level/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek2017
+{
+    static class LevelGenerator
+    {
+        private static string _tileTextureName = "";
+
+        // TODO: Should eventually be data driven
+        //
+        public static Tile[,] GenerateLevel(int width, int height, ContentManager manager)
+        {
+            Tile[,] level = new Tile[height, width];
+            Texture2D tileTexture = manager.Load<Texture2D>(_tileTextureName);
+            
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    level[i, j] = new Tile(tileTexture);
+                }
+            }
+
+            return level;
+        }
+    }
+}
---
+++ Level/Tile.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek2017
+{
+    class Tile : SpriteBase
+    {
+
+        private List<IScriptableObject> _objectsInTile;
+
+        public Tile(Texture2D texture, float x = 0f, float y = 0f, float scale = 1f, float angle = 0f) 
+            : base(texture)
+        {
+            _objectsInTile = new List<IScriptableObject>();
+        }
+
+        public void AddObject(IScriptableObject obj)
+        {
+            _objectsInTile.Add(obj);
+        }
+    }
+}
