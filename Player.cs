7db2135b773d9f139cfa9dbcf8db399f36ad87a4
muhsinking <muhsinking@gmail.com>
2017-07-25 22:41:01 -0700

    Update player.cs

--- Player.cs
+++ Player.cs
@@ -1,22 +1,25 @@
 ﻿using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 
 namespace OneWeek2017
 {
-	public class Player : SpriteClass
+	public class Player : SpriteBase
 	{
 		const string TEXTURENAME = "halberd-badger-nobg";
-		const float speed = 30;
+		float _speed = 100;
 
-		public Player(ContentManager content, float scale) : base(content, TEXTURENAME, scale)
+		public Player(ContentManager content) : base(content, TEXTURENAME)
 		{
 			
 		}
 
 		public void HandleInput()
 		{
+			if (GameStateManager.Instance.CurrentState == GameStateManager.GameState.MovingPlayer)
+			{
+				KeyboardState kb = Keyboard.GetState();
+			}
 		}
 	}
 }
